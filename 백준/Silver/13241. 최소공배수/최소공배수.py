98a07e17bdda2fe208306ca89c6b9ba1563320c5
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-01-28 16:05:05 +0900

    [Silver V] Title: 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,13 @@
+def lcm(n1, n2):
+    max_n = max(n1, n2)
+    min_n = min(n1, n2)
+
+    i = 1
+    while True:
+        if(max_n * i) % min_n == 0:
+            break
+        i += 1
+    return max_n * i
+
+n1, n2 = map(int, input().split())
+print(lcm(n1, n2))
