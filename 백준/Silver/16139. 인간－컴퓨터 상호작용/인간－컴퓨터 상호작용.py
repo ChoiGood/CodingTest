6f1071c04930e2a84bd9ef5d2e7b62ace5d6706d
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-02-01 23:59:24 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 232 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,5 +1,4 @@
 # 인간-컴퓨터 상호작용
-import sys
 # 입력
 word = input() # 입력 받은 문자열
 T = int(input()) # 질문의 개수
@@ -20,7 +19,7 @@ for alpa in alpabet_lower:
     d[alpa] = tmp
 
 for _ in range(T):
-    a, l, r = sys.stdin.readline().rstrip().split()
+    a, l, r = input().split()
     l = int(l)
     r = int(r)
     tmp = d[a] # 해당 알파벳에 대한 누적합에 접근
