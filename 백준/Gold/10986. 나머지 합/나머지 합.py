330fa89bdb8b15aa49ec612a23cba614ebd961b6
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-02-02 14:19:00 +0900

    [Gold III] Title: 나머지 합, Time: 772 ms, Memory: 142292 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -4,19 +4,21 @@ numbers.insert(0, 0)
 
 # 구간합 구하기
 S = [0] * (N + 1)
+
 for i in range(1, N + 1):
     S[i] = S[i - 1] + numbers[i]
-
 # (a + b) % M = a % M + b % M
+cnt_lst = [0] * M
 for i in range(1, N + 1):
     S[i] %= M
+    cnt_lst[S[i]] += 1
 
 # cnt 구하기
 S.pop(0)
 cnt = 0
-cnt += S.count(0)
+cnt += cnt_lst[0]
 for i in range(M):
-    tmp = S.count(i)
+    tmp = cnt_lst[i]
     cnt += (tmp * (tmp - 1)) // 2
 
 print(cnt)
