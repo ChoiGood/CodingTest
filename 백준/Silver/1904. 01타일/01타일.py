2f2b4c40c72d9338c1a45ddfb1ab8b2e50dce798
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-02-06 15:50:11 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 117056 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,43 @@
+# 01 타일
+# N 의 범위 : 1 <= N <= 1,000,000
+# 1번 방식으로 값을 나열하니까 피보나치 수열인 것을 발견!!!
+
+# import sys
+# sys.setrecursionlimit(10**6)
+
+# # 메모이 제이션
+# dp = [0] * 1000001
+# def fibo(n):
+#     if n == 0 or n == 1:
+#         return 1
+#
+#     if dp[n] != 0:
+#         return dp[n]
+#     else:
+#         dp[n] = fibo(n - 1) + fibo(n - 2)
+#
+#     return dp[n]
+#
+# # Process finished with exit code -1073741571 (0xC00000FD)
+
+# 테뷸레이션
+dp2 = [0] * (1000001)
+dp2[0], dp2[1] = 1, 1
+for i in range(2, 1000000 + 1):
+    dp2[i] = (dp2[i - 1] + dp2[i - 2]) % 15746
+
+N = int(input())
+print(dp2[N])
+
+
+# # 1. 수학적으로 해결 ==> 시간 초과
+# import math
+# N = int(input())
+# zeros = list(range(N // 2 + 1))
+#
+# cnt = 0
+# for x in zeros:
+#     cnt += math.comb(N - x, N - 2*x) # ==> 여기서 나는군...
+# print(cnt)
+
+
