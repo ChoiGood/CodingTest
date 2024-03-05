5d7e8390bfcd3f964306f498e40e69d3fab42c3e
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-03-06 01:12:18 +0900

    [Silver I] Title: 카드 구매하기, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -4,14 +4,23 @@ P = list(map(int, input().split())) # 카드팩의 가격
 M = len(P) # 카드팩의 개수
 P.insert(0, 0)
 
-# 일단 이차원 dp로 구현해보기.
-dp = [[0] * (N + 1) for _ in range(M + 1)]
+# # 1. 일단 이차원 dp로 구현해보기.
+# dp = [[0] * (N + 1) for _ in range(M + 1)]
+
+# for i in range(1, M + 1):
+#     for j in range(i):
+#         dp[i][j] = dp[i - 1][j]
+    
+#     for j in range(i, N + 1):
+#         dp[i][j] = max(dp[i - 1][j], dp[i][j - i] + P[i])
+
+# print(dp[M][N])
+
+# 2. 일차원으로 줄여볼 수 있는지에 대해 생각해보기
+dp = [0] * (N + 1)
 
 for i in range(1, M + 1):
-    for j in range(i):
-        dp[i][j] = dp[i - 1][j]
-    
     for j in range(i, N + 1):
-        dp[i][j] = max(dp[i - 1][j], dp[i][j - i] + P[i])
+        dp[j] = max(dp[j], dp[j - i] + P[i])
 
-print(dp[M][N])
+print(dp[N])
