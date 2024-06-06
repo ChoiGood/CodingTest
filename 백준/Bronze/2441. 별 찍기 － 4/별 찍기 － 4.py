0c1500ea3b860b36c82995e25094b290ab0dd80d
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-06-06 23:55:19 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.py
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.py
@@ -1,10 +1,5 @@
-N = int(input())   
-sum = 0                     # 공백 출력 변수
+N = int(input())
 
-for i in range(N, 0, -1) :  # 거꾸 감소하는 반복문
-    for k in range(sum) :   # 공백의 개수 증가하며 출력력
-        print(' ', end = "")
-    for j in range(i) :
-        print('*', end = "")
-    sum += 1
-    print('') 
+for i in range(N):
+    print(' '*i, end='')
+    print('*'*(N-i))
