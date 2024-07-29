30b961d6436c107a58fe3bbef3e9fe8a369d948d
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-07-29 11:05:14 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -6,13 +6,21 @@ int main()
 {
     int A, B;
     
-    while (true)
-    {
-        cin >> A >> B;
-        if (cin.eof())
-            break;
-        else
-            cout << A + B << endl;
+    // while (true)
+    // {
+    //     cin >> A >> B;
+    //     if (cin.eof())
+    //         break;
+    //     else
+    //         cout << A + B << endl;
+    // }
+
+    // while (cin >> A >> B) {
+    //     cout << A + B << "\n";
+    // }
+
+    while (scanf("%d %d", &A, &B) != EOF) {
+        printf("%d\n", A + B);
     }
 
     return 0;
