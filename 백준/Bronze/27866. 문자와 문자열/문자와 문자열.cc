7a829cf333d147c5450166c871c231a2eddcf948
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-08-01 15:05:58 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string word;
+    int index;
+
+    cin >> word >> index;
+    cout << word[index - 1];
+
+    return 0;
+}
