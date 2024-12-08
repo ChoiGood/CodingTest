24696ef3808dfb1bf3bc297742a11c3043da3e45
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-12-08 23:17:08 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,37 +1,20 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <map>
 
 using namespace std;
 
-map<char, int> alpabet_mp;
-vector<int> v;
-
-void Init() {
-    string alpabets = "abcdefghijklmnopqrstuvwxyz";
-    int n = alpabets.size();
-    v.resize(n, 0);
-
-    int idx = 0;
-    for (int i = 0; i < n; i++) {
-        alpabet_mp[alpabets[i]] = idx++;
-    }
-}
-
+string str;
+int cnt[26];
 
 int main() 
 {
-    Init();
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    string S; 
-    cin >> S;
-
-    for (int i = 0; i < S.size(); i++) {
-        v[alpabet_mp[S[i]]]++;
+    cin >> str;
+    for (char a : str) {
+        cnt[a - 'a']++;
     }
-
-    for (int i : v) cout << i << " ";
+    for (int i = 0; i < 26; i++) cout << cnt[i] << " ";
 
     return 0;
 }
