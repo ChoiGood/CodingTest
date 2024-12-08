7f2698bae8dca4c69a43af2c340d077919cdfddc
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-12-09 00:20:37 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -3,36 +3,25 @@
 using namespace std;
 
 int cnt[101];
-
-void print() {
-    for (int c : cnt) {
-        cout << c << " ";
-    }
-    cout << '\n';
-}
-
-int main() 
+int main()
 {
     int A, B, C;
     cin >> A >> B >> C;
-    
-    int fee[4] = {0, A, B, C};
-    
+
     for (int i = 0; i < 3; i++) {
         int a, b;
         cin >> a >> b;
-
-        for (int j = a; j < b; j++) {
-            cnt[j]++;
-        }
+        for (int j = a; j < b; j++) cnt[j]++;
     }
-   
+
     int sum = 0;
-    for (int i = 1; i <= 100; i++) {
-        sum += fee[cnt[i]] * cnt[i];
+    for (int i = 1; i < 100; i++) {
+        if (cnt[i] == 1) sum += A;
+        else if (cnt[i] == 2) sum += B * 2;
+        else if (cnt[i] == 3) sum += C * 3;
     }
-    cout << sum;
 
+    cout << sum << '\n';
 
     return 0;
 }
