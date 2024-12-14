b0c8f2ad8d7330aecc69072f024861937d92a605
ChoiGood <128384646+ChoiGood@users.noreply.github.com>
2024-12-14 23:59:39 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,53 +1,28 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <string>
 
 using namespace std;
 
-vector<string> Split(string &input, string delimeter) {
-    vector<string> result;
-
-    auto start = 0;
-    auto end = input.find(delimeter);
-
-    while (end != string::npos) {
-        result.push_back(input.substr(start, end - start));
-        start = end + delimeter.size();
-        end = input.find(delimeter, start);
-    }
-
-    result.push_back(input.substr(start));
-    return result;
-}
-
 int main()
-{   
-    int N;
-    cin >> N;
-    
-    vector<string> patterns;
-    string bufferflush, fildName;
-    
-    getline(cin, bufferflush);
-    getline(cin, fildName);
+{
+    int n;
+    cin >> n;
 
-    patterns = Split(fildName, "*");
+    string s, ori_s, pre, suf;
+    cin >> ori_s;
 
-    reverse(patterns[1].begin(), patterns[1].end());    
+    int pos = ori_s.find('*');
+    pre = ori_s.substr(0, pos);
+    suf = ori_s.substr(pos + 1);
 
-    for (int i = 0; i < N; i++) {
-        string str;
-        getline(cin, str);
+    for (int i = 0; i < n; i++) {
+        cin >> s;
 
-        string reverse_str = str;
-        reverse(reverse_str.begin(), reverse_str.end());
-        
-        if (str.size() >= (patterns[0].size() + patterns[1].size()) &&(str.find(patterns[0]) == 0) && (reverse_str.find(patterns[1]) == 0)) {
-            cout << "DA" << '\n';
+        if (s.size() >= pre.size() + suf.size() && pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size())) {
+            cout << "DA\n";
         }
         else {
-            cout << "NE" << '\n';
+            cout << "NE\n";
         }
     }
 
